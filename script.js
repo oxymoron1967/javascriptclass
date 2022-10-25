2a0f1ce9b709dca01e4dd8a67aa4b89dc0d7b372
oxymoron1967 <60453252+oxymoron1967@users.noreply.github.com>
2022-10-25 12:13:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+
+$("#joke").append("<p>Q. What do you call a fake noodle? <br /> A. An impasta</p>");
+
+
+$("#noShow+").hide();
+
+$("h1").html("The Best Jokes Ever!");
+
+$("#toggleJokes").css({
+    "background-color": "orange",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
+
+$("#toggleJokes").click(function() {
+  $("#joke").toggle();
+  
+  });
+
+$("#changeback").css({
+  "background-color": "grey",
+  "color": "white",
+  "font-size": "20px",
+  "padding": "10px",
+  "border-radius": "5px",
+  "border": "2px solid black",
+  "margin": "10px"
+})
+
+$("#changeback").click(function() {
+
+  document.body.style.background = "teal";
+});
+  
+   
+let opinion = prompt("Are these the funniest jokes ever? Please answer yes or no.");
+switch(opinion) {
+  case "yes":
+    text = "Agreed.";
+    break;
+  case "no":
+    text = "Sorry";
+    break;
+  default:
+    text = "I don't understand";
+}
+ 
+
+
+
+
+
 // alert('My project is going to be awesome!');
